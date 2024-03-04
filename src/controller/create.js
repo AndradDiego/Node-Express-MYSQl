36f9d434cfef8afd22e4631e56d9c837efca4386
AndradDiego <diegoandradedev@gmail.com>
2024-03-04 13:09:02 -0300

    atribuindo um middleware ao controller "create"

--- src/controller/create.js
+++ src/controller/create.js
@@ -1,11 +1,6 @@
 const database = require("../database")
 const create = (req, res) => {
     const { nome, email, senha, idade } = req.body;
-    database.forEach((item) => {
-        if (item.email == email) {
-            res.status(409).json({ status: `Email ja cadastrado` })
-        }
-    })
     database.push({ nome, email, senha, idade })
     res.status(201).json({ status: `usuario criado!!` })
 }

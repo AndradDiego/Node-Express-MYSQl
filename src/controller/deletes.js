3d8ecc0891194bab14f92cb9ce77253059eae741
AndradDiego <diegoandradedev@gmail.com>
2024-03-04 13:09:22 -0300

    atribuindo um middleware ao controller "delete"

--- src/controller/deletes.js
+++ src/controller/deletes.js
@@ -1,14 +1,7 @@
 const database = require("../database")
+
 const deletes = (req, res) => {
-    const { email } = req.params
-    const usuario = database.find(element => {
-        return element.email == email
-    })
-    if (usuario == -1) {
-        res.status(400).json({ status: `Usuario nao encontrado` })
-    }
-    const posicao = database.indexOf(usuario)
-    database.splice(posicao, 1)
+    database.splice(res.locals.data.posicao, 1)
     res.status(200).json({ status: `Usuario Deletado` })
 }
 module.exports = { deletes }

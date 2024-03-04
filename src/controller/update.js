4cacac3c86d1e68b5085bae94d168d4d833b2f63
AndradDiego <diegoandradedev@gmail.com>
2024-03-04 13:10:07 -0300

    atribuindo um middleware ao controller "update"

--- src/controller/update.js
+++ src/controller/update.js
@@ -1,10 +1,7 @@
 const database = require("../database")
 const update = (req, res) => {
-    const { email } = req.params
-    const usuario = database.find(element => element.email == email)
-    const posicao = database.indexOf(usuario)
     const att = req.body
-    database[posicao] = { ...usuario, ...att }
+    database[res.locals.data.posicao] = { ...database[res.locals.data.posicao], ...att }
     res.status(200).json({ status: `Usuario Atualizado!!!` })
 }
 module.exports = { update }

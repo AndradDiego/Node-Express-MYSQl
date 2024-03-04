83921727668169cec0232d3d3ac2336e28f38754
AndradDiego <diegoandradedev@gmail.com>
2024-03-04 13:12:50 -0300

    atribuindo os middleware a suas devidas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const { deletes } = require("../controller/deletes")
 const { detail } = require("../controller/detail")
 const { list } = require("../controller/list")
 const { update } = require("../controller/update")
+const { verifyemail } = require("../middleware/verifyemail")
+const { Verify } = require("../middleware/verify")
 
 
-route.post("/create", create);
-route.delete("/delete/:email", deletes);
-route.patch("/update/:email", update);
-route.get("/read/:email", detail);
+route.post("/create", verifyemail, create);
+route.delete("/delete/:email", Verify, deletes);
+route.patch("/update/:email", Verify, update);
+route.get("/read/:email", Verify, detail);
 route.get("/list", list)
 module.exports = { route };

0126eb63b3c9deba13a43c9edbafb3f4b765f297
AndradDiego <diegoandradedev@gmail.com>
2024-03-09 16:25:51 -0300

    Criando uma funcao para inserir usuarios no banco de dados

--- src/controller/create.js
+++ src/controller/create.js
@@ -1,8 +1,18 @@
 const database = require("../database")
-const create = (req, res) => {
-    const { nome, email, senha, idade } = req.body;
-    database.push({ nome, email, senha, idade })
-    res.status(201).json({ status: `usuario criado!!` })
+const create = async (req, res) => {
+    const { nome, cpf, email, password } = req.body
+    try {
+        await database.query(`insert into pessoas (nome,cpf,email,password) values('${nome}','${cpf}','${email}','${password}')`)
+        res.status(201).json({ status: `usuario criado!!` })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ status: `Nao cadastrado` })
+
+    }
+
+
+
 }
 module.exports = { create }
 

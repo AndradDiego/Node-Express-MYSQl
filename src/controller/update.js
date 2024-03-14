c27822d57fe71567c5012910d98ae67d91edfdcc
AndradDiego <diegoandradedev@gmail.com>
2024-03-13 21:17:00 -0300

    Implementando a funcao para atualizar um cadastro

--- src/controller/update.js
+++ src/controller/update.js
@@ -1,7 +1,24 @@
 const database = require("../database")
-const update = (req, res) => {
-    const att = req.body
-    database[res.locals.data.posicao] = { ...database[res.locals.data.posicao], ...att }
-    res.status(200).json({ status: `Usuario Atualizado!!!` })
+const update = async (req, res) => {
+    const { email } = req.params
+    const keysName = Object.keys(req.body)
+    const upd = req.body
+    function setDynamicParams() {
+        let aux = ''
+        for (let element = 0; element < keysName.length; element++) {
+            let tempString = `${keysName[element]}='${upd[keysName[element]]}'${element != keysName.length - 1 ? `,` : ``}`
+            aux += tempString
+        }
+        return aux
+    }
+
+    try {
+        await database.query(`UPDATE pessoas SET ${setDynamicParams()} WHERE email = '${email}'`)
+        res.status(200).json({ status: `Usuario Atualizado` })
+
+    } catch (error) {
+
+        res.status(500).json({ status: `error` })
+    }
 }
 module.exports = { update }

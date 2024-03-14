1dcf3faaaa98ba1d6ba4a2b344510ad2629b6c02
AndradDiego <diegoandradedev@gmail.com>
2024-03-13 21:17:43 -0300

    Verificando se o usuario existe no banco de dados

---
+++ src/middleware/verifyCadastro.js
@@ -0,0 +1,15 @@
+const database = require("../database")
+
+const verifyCadastro = async (req, res, next) => {
+    const { email } = req.params
+    try {
+        const result = await database.query(`SELECT * FROM pessoas WHERE email ='${email}'`)
+        if (result) {
+            next();
+        }
+        res.status(401).json({ status: `Usuario nao encontrado!` })
+    } catch (error) {
+        res.status(500).json({ status: `error` })
+    }
+}
+module.exports = { verifyCadastro }

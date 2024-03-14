ef4f94894825380d958497c03933d960515848cd
AndradDiego <diegoandradedev@gmail.com>
2024-03-13 21:16:16 -0300

    Deletando usuario do banco de dados

--- src/controller/deletes.js
+++ src/controller/deletes.js
@@ -1,7 +1,14 @@
 const database = require("../database")
 
-const deletes = (req, res) => {
-    database.splice(res.locals.data.posicao, 1)
-    res.status(200).json({ status: `Usuario Deletado` })
+const deletes = async (req, res) => {
+    const { email } = req.params
+    try {
+        await database.query(`DELETE FROM pessoas WHERE email = '${email}'`)
+        res.status(200).json({ status: `Usuario Deletado` })
+    } catch (error) {
+        res.status(500).json({ status: `error` })
+
+    }
+
 }
 module.exports = { deletes }

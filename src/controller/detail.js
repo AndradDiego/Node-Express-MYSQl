30d7f8b5be5487d2edd433f9f7930ec356e7e11b
AndradDiego <diegoandradedev@gmail.com>
2024-03-13 21:16:30 -0300

    Verificando um usuario no banco de dados

--- src/controller/detail.js
+++ src/controller/detail.js
@@ -1,9 +1,16 @@
 
 const database = require("../database")
-const Verify = require("../middleware/verify")
 
-const detail = (req, res) => {
-    res.status(200).json({ status: `usuario encontrado`, usuario: database[res.locals.data.posicao] })
+const detail = async (req, res) => {
+    const { email } = req.params
+    try {
+        const result = await database.query(`SELECT nome,email,cpf FROM pessoas WHERE email='${email}'`)
+        res.status(200).json(result.rows)
+
+    } catch {
+        res.status(500).json({ status: `error` })
+
+    }
 }
 
 module.exports = { detail }

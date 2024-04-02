96e834c9ff976f95dffc27b896cdd9f43da697f6
AndradDiego <diegoandradedev@gmail.com>
2024-04-02 17:58:15 -0300

    implementando cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
-const {route} = require("./src/routes")
+const { route } = require("./src/routes")
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
-app.use("/",route)
+app.use("/", route)
 
-app.listen(3333,()=>{
+app.listen(3333, () => {
     console.log("servidor on la porta:3333")
 })
